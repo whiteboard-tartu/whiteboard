d586e8ca483e3cfbf8f0dbca1290aefb318e767a
Antony Oroko <antoroko@gmail.com>
2017-07-27 11:35:57 +0300

    Aded quiz classes

---
+++ kool/apps/quiz/answer.py
@@ -0,0 +1,10 @@
+"""Answer
+
+This class represents answers to a question
+"""
+
+class Answer(object):
+	"""docstring for Answer"""
+	def __init__(self, arg):
+		super(Answer, self).__init__()
+		self.arg = arg
---
+++ kool/apps/quiz/question.py
@@ -0,0 +1,10 @@
+"""Question
+
+This class represents questions that make up a quiz
+"""
+
+class Quiz(object):
+	"""docstring for Quiz"""
+	def __init__(self, arg):
+		super(Quiz, self).__init__()
+		self.arg = arg
