2639aad2cae8eaa3bc9d7746804ddc84fa58fec4
Benson Muite <bkmgit@users.noreply.github.com>
2017-07-28 16:46:49 +0300

    Create flatfile.py
    Example showing how to create web interface to simple database.

---
+++ kool/Examples/WebInterfaces/Database/Flatfile/flatfile.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+An example demonstrating how to create a web interface to a flatfile database using wsgi.
+
+More information on WSGI at:
+http://wsgi.readthedocs.io/
+https://docs.python.org/3/library/wsgiref.html
+http://wsgi.tutorial.codepoint.net/
+https://www.toptal.com/python/pythons-wsgi-server-application-interface
+"""
