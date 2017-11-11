5de05d19a913ffe0e51deb2cb246ae46c920629d
Benson Muite <benson_muite@emailplus.org>
2017-11-11 15:33:58 +0200

    updating

--- kool/Examples/WebInterfaces/Database/Flatfile/flatfile.py
+++ kool/Examples/WebInterfaces/Database/Flatfile/flatfile.py
@@ -40,10 +40,10 @@ startpage = """
 </header>
 <h2>Choose an option</h2>
 <form method="post" action="start">
-  Email: <input type="email" name="email"><br>
-  Password: <input type="password" name="pword"><br>
-  <input type="radio" name="role" value="student" checked>Student<br>
-  <input type="radio" name="role" value="educator">Educator<br>
+  <input type="radio" name="update" value="adduser" checked> Add class member<br>
+  <input type="radio" name="update" value="deleteuser"> Delete class member<br>
+  <input type="radio" name="update" value="updateuser"> Update class member grades<br>
+  <input type="radio" name="update" value="showuser"> Show class member grades<br>
   <input type="submit" value="Submit">
 </form>
 <footer>
@@ -53,86 +53,7 @@ startpage = """
 </html>
 """
 
-educator = """
-<html>
-<head>
-<link href="/static/css/style.css" rel="stylesheet">
-<link rel="icon"
-      type="image/png"
-      href="/static/images/logo.png" />
-<title>Kool</title>
-</head>
-<body>
-<header>
-<table style="width:100%">
-  <tr>
-  <th>
-    <img src="/static/images/logo.png" alt="Kool Logo" style="height:128px;">
-  </th>
-  <th colspan="3">
-  <h1>Kool</h1>
-  </th>
-  <th>
-    <img src="/static/images/TartuLogo.png" alt="University of Tartu Logo" style="height:128px;">
-  </th>
-  </tr>
-  </table>
-</header>
-<h2>Choose an option</h2>
-<form method="post" action="educator">
-  <input type="radio" name="update" value="adduser" checked>Add class member<br>
-  <input type="radio" name="update" value="deleteuser">Delete class member<br>
-  <input type="radio" name="update" value="updateuser">Update class member grades<br>
-  <input type="radio" name="update" value="showuser">Show class member grades<br>
-  <input type="radio" name="update" value="makequiz">Make quiz<br>
-  <input type="submit" value="Submit">
-</form>
-<footer>
-<img src="/static/images/TartuMain.jpg" alt="University of Tartu main building" style="height:256px;">
-</footer>
-</body>
-</html>
-"""
-
-student = """
-<html>
-<head>
-<link href="/static/css/style.css" rel="stylesheet">
-<link rel="icon"
-      type="image/png"
-      href="/static/images/logo.png" />
-<title>Kool</title>
-</head>
-<body>
-<header>
-<table style="width:100%">
-  <tr>
-  <th>
-    <img src="/static/images/logo.png" alt="Kool Logo" style="height:128px;">
-  </th>
-  <th colspan="3">
-  <h1>Kool</h1>
-  </th>
-  <th>
-    <img src="/static/images/TartuLogo.png" alt="University of Tartu Logo" style="height:128px;">
-  </th>
-  </tr>
-  </table>
-</header>
-<h2>Choose an option</h2>
-<form method="post" action="student">
-  <input type="radio" name="option" value="showmyscores">Show my scores<br>
-  <input type="radio" name="option" value="takequiz">Take quiz<br>
-  <input type="submit" value="Submit">
-</form>
-<footer>
-<img src="/static/images/TartuMain.jpg" alt="University of Tartu main building" style="height:256px;">
-</footer>
-</body>
-</html>
-"""
-
-addstudent = """
+addname = """
 <html>
 <head>
 <link href="/static/css/style.css" rel="stylesheet">
@@ -177,50 +98,6 @@ addstudent = """
     </html>
 """
 
-updatestudentscore = """
-<html>
-<head>
-<link href="/static/css/style.css" rel="stylesheet">
-<link rel="icon"
-      type="image/png"
-      href="/static/images/logo.png" />
-<title>Kool</title>
-</head>
-    <header>
-    <table style="width:100%">
-      <tr>
-      <th>
-        <img src="/static/images/logo.png" alt="Kool Logo" style="height:128px;">
-      </th>
-      <th colspan="3">
-      <h1>Kool</h1>
-      </th>
-      <th>
-        <img src="/static/images/TartuLogo.png" alt="University of Tartu Logo" style="height:128px;">
-      </th>
-      </tr>
-      </table>
-    </header>
-      <body>
-          <h2>Add user</h2>
-            <form method="post" action="generate">
-              <p>
-                First name: <input type="text" name="fname"><br>
-                Last Name: <input type="text" name="lname"><br>
-                Quiz: <input type="text" name="quiz"><br>
-                Score: <input type="text" name="score">
-              </p>
-              <p>
-                 <input type="submit" value="Submit">
-              </p>
-            </form>
-    <footer>
-    <img src="/static/images/TartuMain.jpg" alt="University of Tartu main building" style="height:256px;">
-    </footer>
-    </body>
-    </html>
-"""
-
 showname = """
 <html>
 <head>
@@ -242,7 +119,7 @@ showname = """
 </html>
 """
 
-delstudent = """
+delname = """
 <html>
 <head>
 <link href="/static/css/style.css" rel="stylesheet">
@@ -386,104 +263,24 @@ haspermission = """
 </html>
 """
 
-showmyscores = """
-<html>
-<head>
-<link href="/static/css/style.css" rel="stylesheet">
-<link rel="icon"
-      type="image/png"
-      href="/static/images/logo.png" />
-<title>Kool</title>
-</head>
-<h1>Kool</h1>
-<body>
-  <p>
-      Placeholder for now
-   </p>
-</body>
-</html>
-"""
-
-makequiz = """
-<html>
-<head>
-<link href="/static/css/style.css" rel="stylesheet">
-<link rel="icon"
-      type="image/png"
-      href="/static/images/logo.png" />
-<title>Kool</title>
-</head>
-<h1>Kool</h1>
-<body>
-  <p>
-      Placeholder for now
-   </p>
-</body>
-</html>
-"""
-
-takequiz = """
-<html>
-<head>
-<link href="/static/css/style.css" rel="stylesheet">
-<link rel="icon"
-      type="image/png"
-      href="/static/images/logo.png" />
-<title>Kool</title>
-</head>
-<h1>Kool</h1>
-<body>
-  <p>
-      Placeholder for now
-   </p>
-</body>
-</html>
-"""
-
 class accessdatabase(object):
     @cherrypy.expose
     def index(self):
         return startpage
 
     @cherrypy.expose
-    def start(self,email,pword,role):
-      # check password is correct one for supplied email address
-        pwordemail='match'
-        if pwordemail=='match' & role=='educator':
-            return educator
-        elif pwordemail=='match' & role=='student':
-            return student
+    def start(self,update):
+        if update=='adduser':
+            return addname
+        elif update=='deleteuser':
+            return delname
+        elif update=='showuser':
+            return showuser
         else:
-            return startpage
+            return addgrade
 
     @cherrypy.expose
-    def student(self,option):
-        if option=='showmyscores':
-            return showmyscores
-        else:
-            return takequiz
-
-    @cherrypy.expose
-    def educator(self,option):
-        if option=='addstudent':
-            return addstudent
-        elif option=='delstudent':
-            return delstudent
-        elif option=='makequiz':
-            return makequiz
-        else:
-            return updatestudentscore
-
-    @cherrypy.expose
-    def updatestudentscore(self,firstname,lastname,quiz,score):
-        #check firstname, lastname, quiz and score match
-        if options!='match':
-            return addstudent
-        else:
-            return student
-
-    @cherrypy.expose
-    def addstudent(self,fname,lname,email,pword1,pword2):
+    def generate(self,fname,lname,email,pword1,pword2):
         # Always escape user input to avoid script injection
         fname = escape(fname)
         lname = escape(lname)
@@ -495,7 +292,7 @@ class accessdatabase(object):
             if pword1==pword2:
                 match='Passwords match, record added'
             else:
-                match='Passwords do not match, enter again'
+                match = 'Passwords do not match, enter again'
 
         response_body = showname % { # Fill the above html template in
           'fname': fname or 'No entry',
